eb85eeabaf989522cae73199656d6144f4bd5146
DarynaP <darynaa@hotmail.com>
2023-03-14 19:15:40 +0000

    app fix

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def update():
 ########################################
 
 if __name__ == "__main__":
-    app.run(host=‘0.0.0.0’, debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
